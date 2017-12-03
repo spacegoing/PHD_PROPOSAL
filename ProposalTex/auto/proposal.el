1be5823b5ea543f73b4a055eea95c98b21b053a1
spacegoing <spacegoing@gmail.com>
2017-12-03 18:39:06 +0800

    [New] Intro aim1

--- ProposalTex/auto/proposal.el
+++ ProposalTex/auto/proposal.el
@@ -5,10 +5,14 @@
     "capsule"
     "donahue2015long"
     "SDT"
+    "hibon1997arma"
     "gehring2017convolutional"
     "qin2017dual"
     "johnson2016composing"
     "hsu2017unsupervised"
-    "narayanaswamy2017learning"))
+    "narayanaswamy2017learning"
+    "brockwell2013time"
+    "koller2009probabilistic"
+    "murphy2012machine"))
  :bibtex)
 
---
+++ ProposalTex/auto/usyd_phd_proposal.el
@@ -0,0 +1,45 @@
+(TeX-add-style-hook
+ "usyd_phd_proposal"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin={1.5cm,2cm}") ("fontenc" "T1") ("inputenc" "utf8") ("StyFiles/natbib" "sort" "numbers")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "tabularx"
+    "amsmath"
+    "amssymb"
+    "tikz"
+    "booktabs"
+    "float"
+    "geometry"
+    "multicol"
+    "tabto"
+    "pdflscape"
+    "graphicx"
+    "array"
+    "fontenc"
+    "inputenc"
+    "charter"
+    "environ"
+    "StyFiles/natbib"
+    "StyFiles/natbibspacing")
+   (TeX-add-symbols
+    "endlr"
+    "matamp")
+   (LaTeX-add-labels
+    "sec:synop"
+    "sec:intro"
+    "sec:contrib"
+    "sec:method"
+    "NCRA-TIFR")
+   (LaTeX-add-environments
+    '("vtimeline" LaTeX-env-args ["argument"] 0))
+   (LaTeX-add-bibliographies
+    "proposal.bib")
+   (LaTeX-add-counters
+    "lines"
+    "vtml"))
+ :latex)
+
